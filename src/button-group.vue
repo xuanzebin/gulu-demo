bf0ab0af0ae8bfbe2e90bc8dfee1feb4dbb77827
xuanzebin <xuanzebin@126.com>
2018-09-09 11:37:40 +0800

    完成了单Vue组件g-button-group，并解决了边框合并的问题

---
+++ src/button-group.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="g-button-group">
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default{
+
+    }
+</script>
+<style lang="scss">
+    .g-button-group{
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        &>.g-button{
+            &:not(:first-child){
+                margin-left: -1px;
+            }
+            border-radius: 0;
+            &:first-child{
+                border-top-left-radius:var(--border-radius);
+                border-bottom-left-radius: var(--border-radius);
+            }
+            &:last-child{
+                border-top-right-radius: var(--border-radius);
+                border-bottom-right-radius: var(--border-radius);
+            }
+            &:hover{
+                position:relative;
+                z-index:1;
+            }
+        }
+    }
+</style>
