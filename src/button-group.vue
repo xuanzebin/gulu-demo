081225d7abeb4d5629617cc5263e16395bbab124
xuanzebin <xuanzebin@126.com>
2018-09-09 11:41:49 +0800

    给g-button-group组件添加了错误提醒

--- src/button-group.vue
+++ src/button-group.vue
@@ -5,7 +5,14 @@
 </template>
 <script>
     export default{
-
+        mounted(){
+            for (let node of this.$el.children){
+                let name=node.nodeName.toLowerCase()
+                if (name!=='button'){
+                    console.warn(`g-button-group的子元素应该全是g-button，但是你的子元素中含有非g-button元素${name}`)
+                }
+            }
+        }
     }
 </script>
 <style lang="scss">
