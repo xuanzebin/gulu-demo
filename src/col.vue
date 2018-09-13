e76a51d95381003785e401cb1b4e976a388c805a
xuanzebin <xuanzebin@126.com>
2018-09-13 21:57:15 +0800

    完成了input的全部自动化测试

---
+++ src/col.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="col" :class="[span && `col-${span}`]">
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default{
+        props:{
+            span:{
+                type:[Number,String]
+            }
+        }
+    }
+</script>
+<style scoped lang="scss">
+    .col{
+        height:100px;
+        width:50%;
+        border:1px solid black;
+
+        $class:col-;
+        @for $n from 1 through 24{
+            &.#{$class}#{$n}{
+                width:($n/24)*100%;
+            }
+        }
+    }
+</style>
