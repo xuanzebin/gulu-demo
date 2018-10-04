97675de430fa88cfc123262c39ee072a0cfe6086
xuanzebin <xuanzebin@126.com>
2018-10-04 17:26:54 +0800

    完成了toast组件的初始化和基本功能

--- src/app.js
+++ src/app.js
@@ -10,7 +10,8 @@ import Header from './header'
 import Content from './content'
 import Footer from './footer'
 import Sider from './sider'
-
+import Toast from './toast'
+import plugin from "./plugin";
 
 Vue.component('g-button',Button)
 Vue.component('g-icon',Icon)
@@ -23,7 +24,8 @@ Vue.component('g-content',Content)
 Vue.component('g-header',Header)
 Vue.component('g-footer',Footer)
 Vue.component('g-sider',Sider)
-
+Vue.component('g-toast',Toast)
+Vue.use(plugin)
 
 new Vue({
     el:'#app',
@@ -36,6 +38,9 @@ new Vue({
     methods:{
         inputKey(e){
             console.log(e.target.value)
+        },
+        showToast(){
+            this.$toast('我是message')
         }
     }
 })
---
+++ src/toast.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="g-toast">
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default {
+        name:'GuluToast'
+    }
+</script>
+<style scoped lang="scss">
+    $g-toast-bg:rgba(0,0,0,0.74);
+    $g-toast-shadow:0 0 3px 0 rgba(0,0,0,0.50);
+    $g-toast-font-size:14px;
+    $g-toast-line-height:2.0;
+    .g-toast{
+        position:fixed;top:0;left:50%;transform: translateX(-50%);color:white;
+        padding:0 16px;border-radius: 4px;background: $g-toast-bg;
+        box-shadow: $g-toast-shadow;font-size:$g-toast-font-size;line-height: $g-toast-line-height;
+    }
+</style>
