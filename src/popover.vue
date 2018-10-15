4ad1724a49b68df88cc487b6e4188bd445bdcba6
xuanzebin <xuanzebin@126.com>
2018-10-15 18:32:23 +0800

    实现了popover组件的基本功能

--- src/popover.vue
+++ src/popover.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="popover">
-        <div class="contentWrapper">
+    <div class="popover" @click="popoverTrigger">
+        <div class="contentWrapper" v-if="visible" @click.stop>
             <slot name="content"></slot>
         </div>
         <slot></slot>
@@ -8,14 +8,38 @@
 </template>
 <script>
     export default {
-        name: "GuluPopover"
+        name: "GuluPopover",
+        data(){
+            return {
+                visible:false
+            }
+        },
+        methods:{
+            popoverTrigger(){
+                this.visible=!this.visible
+                if (this.visible){
+                    this.$nextTick(()=>{
+                        let eventHandler=()=>{
+                            this.visible=false
+                            document.removeEventListener('click',eventHandler)
+                        }
+                        document.addEventListener('click',eventHandler)
+                    })
+                }
+            }
+        }
     }
 </script>
 
 <style scoped lang="scss">
 .popover{
+    margin:50px;
     display:inline-block;
+    position:relative;
     >.contentWrapper{
+        position:absolute;
+        bottom:100%;
+        left:0;
         border:1px red solid;
         padding:5px;
     }
