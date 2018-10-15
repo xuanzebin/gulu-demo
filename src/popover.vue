ea14bad13b83e6fa19f7c2e1ed6a4e9b4aa11f1e
xuanzebin <xuanzebin@126.com>
2018-10-15 18:25:17 +0800

    完成了Popover组件的初始化

---
+++ src/popover.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="popover">
+        <div class="contentWrapper">
+            <slot name="content"></slot>
+        </div>
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default {
+        name: "GuluPopover"
+    }
+</script>
+
+<style scoped lang="scss">
+.popover{
+    display:inline-block;
+    >.contentWrapper{
+        border:1px red solid;
+        padding:5px;
+    }
+}
+</style>
