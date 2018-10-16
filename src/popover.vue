dae29e4621805cd1584de645d2f4fd0b515960cc
xuanzebin <xuanzebin@126.com>
2018-10-16 23:47:19 +0800

    解决了popover组件因父元素overflow:hidden;而造成的bug

--- src/popover.vue
+++ src/popover.vue
@@ -1,9 +1,12 @@
 <template>
     <div class="popover" @click="popoverTrigger">
-        <div class="contentWrapper" v-if="visible" @click.stop>
+        <div class="contentWrapper" v-if="visible"
+        ref="contentWrapper">
             <slot name="content"></slot>
         </div>
-        <slot></slot>
+        <span class="triggerButton" ref="triggerButton">
+            <slot></slot>
+        </span>
     </div>
 </template>
 <script>
@@ -19,6 +22,10 @@
                 this.visible=!this.visible
                 if (this.visible){
                     this.$nextTick(()=>{
+                        document.body.appendChild(this.$refs.contentWrapper)
+                        let {width,height,left,top}=this.$refs.triggerButton.getBoundingClientRect()
+                        this.$refs.contentWrapper.style.left=left+window.scrollY+'px'
+                        this.$refs.contentWrapper.style.top=top+window.scrollX+'px'
                         let eventHandler=()=>{
                             this.visible=false
                             document.removeEventListener('click',eventHandler)
@@ -36,12 +43,11 @@
     margin:50px;
     display:inline-block;
     position:relative;
-    >.contentWrapper{
-        position:absolute;
-        bottom:100%;
-        left:0;
-        border:1px red solid;
-        padding:5px;
-    }
+}
+.contentWrapper{
+    position:absolute;
+    border:1px red solid;
+    padding:5px;
+    transform: translateY(-100%);
 }
 </style>
